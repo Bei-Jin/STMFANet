ac437a3f08ec7d96a2dbe5aec61d1eb6e4978435
Murphy <21143766+Bei-Jin@users.noreply.github.com>
2021-02-03 11:03:41 +0800

    Update base_model.py

--- wavenet_models/base_model.py
+++ wavenet_models/base_model.py
@@ -37,23 +37,3 @@ class BaseModel():
 
     def save(self, label, epoch):
         pass
-
-    def save_network(self, network, network_label, epoch_label, gpu_ids):
-        save_filename = '%s_net_%s.pth' % (epoch_label, network_label)
-        save_path = os.path.join(self.save_dir, save_filename)
-        torch.save(network.cpu().state_dict(), save_path)
-        if len(gpu_ids) and torch.cuda.is_available():
-            network.cuda(device_id=gpu_ids[0])
-
-    # helper loading function that can be used by subclasses
-    def load_network(self, network, network_label, epoch_label):
-        save_filename = '%s_net_%s.pth' % (epoch_label, network_label)
-        save_path = os.path.join(self.save_dir, save_filename)
-        network.load_state_dict(torch.load(save_path))
-
-    # update learning rate (called once every epoch)
-    def update_learning_rate(self):
-        for scheduler in self.schedulers:
-            scheduler.step()
-        lr = self.optimizers[0].param_groups[0]['lr']
-        print('learning rate = %.7f' % lr)
